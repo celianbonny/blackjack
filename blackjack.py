53d248823ed633bafe33839344a5eec8f54bb122
celianbonny <celian_bonny@etu.ube.fr>
2026-09-11 09:03:10 +0200

    commit choix menu
    implentation du choix au debut

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,15 @@
-print("hello")
+print("1. JOUER")
+print("2. QUITTER")
+
+while True:
+    choix = input("Choisissez 1 ou 2 : ")
+    if choix in ['1', '2']:
+        break
+    else:
+        print("Choix incorrect !")
+
+print(f"Vous avez choisi l'option {choix}")
+
+if (choix==1):
+    print("vous avez 100 jetons")
+
