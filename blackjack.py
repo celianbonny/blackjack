204761af1afc833d726012ad42b98f0d9661c0a0
celianbonny <celian_bonny@etu.ube.fr>
2026-09-11 09:56:07 +0200

    commit base black jack

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,18 @@
+import random
+
+depart=100
+
+def creer_paquet():
+    #Crée un jeu de 52 cartes standard.
+    couleurs = ['Cœur', 'Carreau', 'Trèfle', 'Pique']
+    valeurs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As']
+    # Crée une liste de tuples (valeur, couleur)
+    paquet = [(valeur, couleur) for couleur in couleurs for valeur in valeurs]
+    random.shuffle(paquet)
+    return paquet
+
+
+
 print("1. JOUER")
 print("2. QUITTER")
 
@@ -10,6 +25,15 @@ while True:
 
 print(f"Vous avez choisi l'option {choix}")
 
-if (choix==1):
+if choix=='1':
     print("vous avez 100 jetons")
+    mise = int(input("choisie ta mise : "))
+    print (f"vous avez mise {mise} jetons")
+    gain = int(depart-mise)
+    print(gain)
+    print(paquet)
+
+else : 
+    print("relance")
+
 
